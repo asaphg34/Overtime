79b702c977236022d5e9ccda4338f7478da27d64
asaphg34 <asaph.garcia@nwktc.edu>
2024-03-20 08:52:37 -0600

    Initial Commit

---
+++ Overtime Challenge/Overtime_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Overtime_ChallengeApp.swift
+//  Overtime Challenge
+//
+//  Created by Asaph Garcia on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Overtime_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Overtime ChallengeUITests/Overtime_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Overtime_ChallengeUITestsLaunchTests.swift
+//  Overtime ChallengeUITests
+//
+//  Created by Asaph Garcia on 3/20/24.
+//
+
+import XCTest
+
+final class Overtime_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
